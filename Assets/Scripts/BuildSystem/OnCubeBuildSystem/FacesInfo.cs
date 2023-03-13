2214e4257b6f7ffc271d2f6eedf95ede1585f3ef
snapp <snapp@snapp.co>
2023-03-13 19:33:02 +0300

    added new type of block
    the possibility of easy customization by the game designer has been tested

--- Assets/Scripts/BuildSystem/OnCubeBuildSystem/FacesInfo.cs
+++ Assets/Scripts/BuildSystem/OnCubeBuildSystem/FacesInfo.cs
@@ -2,5 +2,11 @@
 
 public class FacesInfo : MonoBehaviour
 {
+    [Header( "5 -> -z")]
+    [Header("4 -> -y")]
+    [Header("3 -> -x")]
+    [Header("2 -> z")]
+    [Header("1 -> y")]
+    [Header("0 -> x")]
     [SerializeField] public bool[] _facesInfo = new bool[6];
 }
